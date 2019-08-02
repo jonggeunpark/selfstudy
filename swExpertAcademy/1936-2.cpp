9c1ffdd429a336023ebeb2ece2fac52a64afa588
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2019-08-02 14:48:05 +0900

    Create 1936-2.cpp

---
+++ swExpertAcademy/1936-2.cpp
@@ -0,0 +1,17 @@
+//swExpertAcademy
+//2019
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int A, B;
+
+	cin >> A >> B;
+
+	if (A % 3 == (B + 1) % 3)
+		cout << "A";
+	else if (B % 3 == (A + 1) % 3)
+		cout << "B";
+}

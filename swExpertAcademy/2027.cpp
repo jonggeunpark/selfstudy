54559057ded1c05ecdcac937c270fc51ca2d4f9a
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2019-08-02 19:16:29 +0900

    Create 2027.cpp

---
+++ swExpertAcademy/2027.cpp
@@ -0,0 +1,16 @@
+//swExpertAcademy
+//2027
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "#++++" << endl;
+	cout << "+#+++" << endl;
+	cout << "++#++" << endl;
+	cout << "+++#+" << endl;
+	cout << "++++#" << endl;
+
+	return 0;
+}

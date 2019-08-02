8618f46bd8a2a5b893c67e10ffe7412596dc4464
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2019-08-02 19:17:05 +0900

    Create 2070.cpp

---
+++ swExpertAcademy/2070.cpp
@@ -0,0 +1,26 @@
+//swExpertAcademy
+//2070
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t,a,b;
+	cin >> t;
+
+	for (int i = 1; i <= t;i++)
+	{
+		cin >> a >> b;
+		cout << "#" << i << " ";
+
+		if (a > b)
+			cout << ">" << endl;
+		else if (a < b)
+			cout << "<" << endl;
+		else
+			cout << "=" << endl;
+	}
+
+	return 0;
+}

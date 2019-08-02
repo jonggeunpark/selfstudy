8ce1ebdb550cf1ad40fcea46d12dd8aab130da4d
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2019-08-02 19:16:26 +0900

    Create 2025.cpp

---
+++ swExpertAcademy/2025.cpp
@@ -0,0 +1,20 @@
+//swExpertAcademy
+//2025
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, tmp = 0;
+	cin >> n;
+
+	for (int i = 0;i <= n;i++)
+	{
+		tmp += i;
+	}
+
+	cout << tmp;
+
+	return 0;
+}

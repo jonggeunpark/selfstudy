2f093340e261664f11eb957d9c061df2a53a58b0
박종rms <whdrms5826@naver.com>
2019-08-16 00:14:57 +0900

    Revert "19/08/15"
    This reverts commit 47eea51b833d32b59f5c94856607d24bd51bc219.

--- swExpertAcademy/D3/1860.cpp
+++ swExpertAcademy/D3/1860.cpp
@@ -7,28 +7,28 @@ using namespace std;
 
 int main()
 {
-	int t, n, m, k, tmp, eaten_bread;
+	int t, n, m, k, tmp, time;
+	int bread, count;
 	bool check;
 	vector <int> v;
-
-
 	cin >> t;
 
 	for (int i = 1;i <= t;i++)
 	{
 		v.clear();
 		check = true;
-		eaten_bread = 0;
+		count = 0;
+		bread = 0;
 
 		cin >> n >> m >> k;
 
-		for (int i = 0;i < n;i++) // 벡터에 대입
+		for (int i = 0;i < n;i++)
 		{
 			cin >> tmp;
 			v.push_back(tmp);
 		}
 
-		for (int i = 0;i < n;i++) // 오름차순 정렬
+		for (int i = 0;i < n;i++)
 			for (int j = 0;j < n - i - 1;j++)
 			{
 				if (v[j] > v[j + 1])
@@ -39,19 +39,32 @@ int main()
 				}
 			}
 
-		for (int i = 0;i < n;i++)
+		for (int time = 0;time < 11112;time++)
 		{
-			if (v[i] / m * k - eaten_bread <= 0)
-				check = false;
-			else
-				eaten_bread++;
+			if (time / m >= 1 && time%m == 0)
+				bread += k;
+
+			for (int i = 0;i < n;i++)
+			{
+				if (time == v[i])
+				{
+					if (bread == 0)
+					{
+						check = false;
+						break;
+					}
+					bread -= 1;
+				}
+			}
+			if (check == false)
+				break;
 		}
 
 		cout << "#" << i;
 		if (check == true)
 			cout << " Possible" << endl;
 		else
-			cout << " Impossible" << endl;
+			cout << " Imossible" << endl;
 	}
 	return 0;
 }

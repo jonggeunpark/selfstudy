212574fea5a15aad76914170a6bd8bf48769f162
박종rms <whdrms5826@naver.com>
2019-08-16 23:23:57 +0900

    2019/08/16
    에라토스테네스의 체
    메모리 부족 오류

--- swExpertAcademy/D3/3131.cpp
+++ swExpertAcademy/D3/3131.cpp
@@ -1,24 +1,34 @@
 //swExpertAcademy
 //3131
+//에라토스테네스의 체
 
 #include <iostream>
-#include <math.h>
+#define SIZE 1000000 // 스택 오버플로 발생
 using namespace std;
 
 int main()
 {
+	int arr[SIZE];
+	fill_n(arr, SIZE, 1);
 	bool check;
-	for (int i = 2;i <= pow(10, 6);i++)
+
+	for (int i = 2;i <= 1000;i++)
 	{
-		check = true;
-		for (int j = 2;j < i;j++)
-			if (i % j == 0)
-			{
-				check = false;
-				break;
-			}
-		if (check == true)
-			cout << i << " ";
+		check = false;
+		for (int j = i;j < SIZE;j++)
+		{
+			if (arr[j] == 1)
+				if ((j + 1) % i == 0)
+					arr[j] = 0;
+				else if (check == true)
+				{
+					i = j + 1;
+				}
+		}
 	}
+	for (int i = 1;i < SIZE;i++)
+		if (arr[i] == 1)
+			cout << i + 1 << " ";
+
 	return 0;
 }

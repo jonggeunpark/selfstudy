fa31fefe1fa474a8c32d24620b4a66d4a47cc61f
박종rms <whdrms5826@naver.com>
2019-08-17 23:59:04 +0900

    BOJ10950 A+B - 3

---
+++ baekjoon/BOJ_10950.cpp
@@ -0,0 +1,18 @@
+//Baekjoon online judge
+//10590
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, a, b;
+	cin >> t;
+	for (int i = 0;i < t;i++)
+	{
+		cin >> a >> b;
+		cout << a + b << endl;
+	}
+
+	return 0;
+}

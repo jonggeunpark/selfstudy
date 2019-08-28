20946b834479f48ddac161c95ae85a5e028496ff
박종rms <whdrms5826@naver.com>
2019-08-28 18:17:57 +0900

    BOJ1929 소수 구하기
    에라토스테네스의 체

--- baekjoon/math 2/BOJ_1929.cpp
+++ baekjoon/math 2/BOJ_1929.cpp
@@ -1,31 +1,37 @@
 //Baekjoon online judge
 //1929
+//에라토스테네스의 체
 
 #include <iostream>
+#include <math.h>
 using namespace std;
-
-bool check(int a)
-{
-	int count = 0;
-	for (int i = 1; i <= a; i++)
-	{
-		if (a%i == 0)
-			count++;
-	}
-	if (count == 2)
-		return true;
-	else return false;
-}
 
 int main()
 {
 	int m, n;
 	cin >> m >> n;
 
+	int *arr = new int[n + 2 ];
+	fill_n(arr, n + 2 , true);
+	arr[1] = false;
+
+	for (int i = 2; i <= sqrt(n); i++)
+	{
+		if (arr[i] == true)
+		{
+			for (int j = i * i; j <= n; j++)
+			{
+				if (j%i == 0)
+					arr[j] = false;
+			}
+		}
+	}
+
 	for (int i = m; i <= n; i++)
 	{
-		if (check(i) == true)
-			cout << i << endl;
+		if (arr[i] == true)
+			cout << i << '\n';
 	}
+
 	return 0;
 }

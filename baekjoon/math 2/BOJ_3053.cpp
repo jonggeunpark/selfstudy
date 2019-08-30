ebc9cfdd36d5a38e2e3ff5b6e2d487f8879f3752
박종rms <whdrms5826@naver.com>
2019-08-30 22:37:15 +0900

    BOJ3053 택시 기하학
    택시 기하학

---
+++ baekjoon/math 2/BOJ_3053.cpp
@@ -0,0 +1,18 @@
+//Baekjoon online judge
+//3053
+
+#include <iostream>
+using namespace std;
+#define PI 3.14159265358979
+
+int main()
+{
+	double r;
+	cin >> r;
+
+	cout.precision(6);
+	cout << fixed << r * r* PI << endl;
+	cout << fixed << r * r * 2;
+
+	return 0;
+}

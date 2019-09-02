e2d3f59e3f9f044af38be214d3a93a0bf353d63d
박종rms <whdrms5826@naver.com>
2019-09-02 23:40:47 +0900

    BOJ2750 수 정렬하기

---
+++ baekjoon/sort/BOJ_2750.cpp
@@ -0,0 +1,38 @@
+//Baekjoon online judge
+//2750
+
+#include <iostream>
+using namespace std;
+
+void sort(int n, int* arr)
+{
+	int tmp;
+	for (int i = 0; i < n - 1; i++)
+		for (int j = 0; j < n - i - 1; j++)
+			if (arr[j] > arr[j + 1])
+			{
+				tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+}
+
+int main()
+{
+	int n, tmp;
+	cin >> n;
+	int* arr = new int[n];
+	
+	for (int i = 0; i < n; i++)
+	{
+		cin >> tmp;
+		arr[i] = tmp;
+	}
+
+	sort(n,arr);
+
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << endl;
+
+	return 0;
+}

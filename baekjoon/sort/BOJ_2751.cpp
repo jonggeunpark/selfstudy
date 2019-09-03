ca7c50f59abcf54970505eab1ee3b94e9a362750
박종rms <whdrms5826@naver.com>
2019-09-03 22:20:00 +0900

    BOJ2751 수 정렬하기2
    병합정렬(merge sort)

--- baekjoon/sort/BOJ_2751.cpp
+++ baekjoon/sort/BOJ_2751.cpp
@@ -2,40 +2,76 @@
 //2751
 
 #include <iostream>
-#include <vector>
 using namespace std;
 
-void sort(int n,vector<int> &v)
+void mergeTwoArea(int arr[],int arr2[], int left, int mid, int right)
 {
-	int tmp;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n - i - 1; j++)
-		{
-			if (v[j] > v[j + 1])
-			{
-				tmp = v[j];
-				v[j] = v[j + 1];
-				v[j + 1] = tmp;
-			}
-		}
+	int fIdx = left;
+	int rIdx = mid + 1;
+	int sIdx = left;
+	int i;
+
+	while (fIdx <= mid && rIdx <= right)
+	{
+		if (arr[fIdx] <= arr[rIdx])
+			arr2[sIdx] = arr[fIdx++];
+		else
+			arr2[sIdx] = arr[rIdx++];
+
+		sIdx++;
+	}
+
+	if (fIdx > mid)
+	{
+		for (int i = rIdx; i <= right; i++, sIdx++)
+			arr2[sIdx] = arr[i];
+	}
+	else
+	{
+		for (i = fIdx; i <= mid; i++,sIdx++)
+			arr2[sIdx] = arr[i];
+	}
+
+	for (i = left; i <= right; i++)
+		 arr[i] = arr2[i];
+}
+
+void mergeSort(int arr[],int arr2[], int left, int right)
+{
+	int mid;
+
+	if (left < right)
+	{
+		mid = (left + right) / 2;
+		mergeSort(arr, arr2,left, mid);
+		mergeSort(arr, arr2,mid + 1, right);
+		mergeTwoArea(arr,arr2, left, mid, right);
+	}
 }
 
 int main()
 {
+	cin.tie(NULL);
+
 	int n, tmp;
 	cin >> n;
-	vector<int> v;
+	int* arr = new int[n];
+	int* arr2 = (int*)malloc(sizeof(int)*(1000000));
 
 	for (int i = 0; i < n; i++)
 	{
 		cin >> tmp;
-		v.push_back(tmp);
+		arr[i] = tmp;
 	}
-	sort(n, v);
+
+	mergeSort(arr,arr2, 0, n - 1);
 
 	for (int i = 0; i < n; i++)
 	{
-		cout << v[i] << endl;
+		cout << arr[i] << '\n';
 	}
+
+	free(arr2);
+
 	return 0;
 }
